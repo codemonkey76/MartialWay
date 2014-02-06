0ad32d3a28f157c080a2f3ce7487cc1fe2747bbe
Shane Poppleton <shane@bjja.com.au>
2014-02-06 22:28:28 +1000

    Initial Application Prototype

---
+++ MartialWay/MartialWay.Server/DataSources/ApplicationData/Student.lsml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class Student
+    {
+        partial void FullName_Compute(ref string result)
+        {
+            result = this.LastName + ", " + this.FirstName;
+        }
+    }
+}
