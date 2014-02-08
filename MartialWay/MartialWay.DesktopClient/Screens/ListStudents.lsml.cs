b5d294b534bbefb0366f78c5707b7e5d3f7906fe
Shane Poppleton <shane@bjja.com.au>
2014-02-08 11:22:21 +1000

    Hepas of changes again

---
+++ MartialWay/MartialWay.DesktopClient/Screens/ListStudents.lsml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+namespace LightSwitchApplication
+{
+    public partial class ListStudents
+    {
+        partial void NewNextOfKin_Execute()
+        {
+            this.Application.ShowMaintainNextOfKins();
+        }
+
+        partial void gridAddAndEditNew_CanExecute(ref bool result)
+        {
+            // Write your code here.
+
+        }
+
+        partial void gridAddAndEditNew_Execute()
+        {
+            Student newStudent = this.Students.AddNew();
+            this.Students.SelectedItem = newStudent;
+            this.OpenModalWindow("StudentEditDialog");
+        }
+
+        partial void gridEditSelected_CanExecute(ref bool result)
+        {
+            // Write your code here.
+
+        }
+
+        partial void gridEditSelected_Execute()
+        {
+            this.OpenModalWindow("StudentEditDialog");
+        }
+    }
+}
---
+++ MartialWay/MartialWay.Server/DataSources/ApplicationData/Rank.lsml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class Rank
+    {
+        partial void BeltName_Compute(ref string result)
+        {
+            result = this.Belt + " Belt";
+            if (this.Stripes != null)
+                if (this.Stripes.Length > 0)
+                    result += " - (" + this.Stripes + ")";
+        }
+
+        partial void MinimumTime_Compute(ref string result)
+        {
+            if (this.MinTimeInBelt < 12)
+                result = this.MinTimeInBelt.ToString() + " months";
+            else
+            {
+                result = (this.MinTimeInBelt / 12).ToString() + " years";
+                if (this.MinTimeInBelt % 12 != 0)
+                    result += " " + (this.MinTimeInBelt % 12).ToString() + " months";
+            }
+        }
+    }
+}
