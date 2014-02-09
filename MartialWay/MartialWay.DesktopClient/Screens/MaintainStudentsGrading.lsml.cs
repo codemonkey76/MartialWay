e9bf07d2819d2e61016a4e5ffdcffb90bca25090
Shane Poppleton <shane@bjja.com.au>
2014-02-10 07:35:54 +1000

    Added promotions

---
+++ MartialWay/MartialWay.DesktopClient/Screens/MaintainStudentsGrading.lsml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+namespace LightSwitchApplication
+{
+    public partial class MaintainStudentsGrading
+    {
+        partial void AddAllActiveStudents_Execute()
+        {
+            foreach (Student s in ActiveStudents)
+            {
+                StudentGrading sg = StudentGradings.AddNew();
+                sg.Grading = this.Gradings.SelectedItem;
+                sg.Student = s;
+                sg.GradingDecision = 0;
+                sg.InvitationGiven = false;
+                //sg.RankToGradeFor = GetNextRank();
+                
+            }
+            
+        }
+        
+    }
+}
---
+++ MartialWay/MartialWay.HTMLClient/Screens/BrowseStudents.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.BrowseStudents.FullName_postRender = function (element, contentItem) {
+    // Write code here.
+    return contentItem.screen.Students.selectedItem.ModifiedBy;
+};
